05a0d070b392a2565c76513c3d55ba22b8a8d537
Mehul Aneja <97171516+Mehul-Aneja@users.noreply.github.com>
2024-07-27 16:40:07 -0400

    visual update

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,19 +29,19 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <main className="pb-10 md:px-20 lg:px-40 dark:bg-gradient-to-b dark:from-white dark:via-purple-500 dark:to-purple-900">
+      <main className="pb-10 md:px-20 lg:px-40 dark:bg-gradient-to-b dark:from-white dark:via-cyan-500 dark:to-cyan-900">
         {init && !darkMode && ( <div className="absolute top-0 left-0 w-full h-full -z-10"> <Particles options={particlesBackgroundOptions} /> </div> )}
         <section className="min-h-screen">
 
           <nav className="pt-8 flex justify-between">
-            <div className="ml-3 transition ease-in-out delay-10 hover:scale-110"> <a className="rounded-full border bg-gradient-to-r from-purple-500 to-teal-600 text-black px-4 py-2 rounded-md text-lg dark:text-white dark:from-black dark:to-black" href="https://drive.google.com/file/d/1JGsI_js11UJGpBuvtI92cQpkpH7OmUb_/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a> </div>
-            <div className="mr-3"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="text-purple-500 cursor-pointer text-3xl transition ease-in-out delay-50 hover:scale-110 dark:text-slate-900" /></div>
+            <div className="ml-3 transition ease-in-out delay-10 hover:scale-110"> <a className="rounded-full border bg-gradient-to-r from-cyan-500 to-teal-900 text-black px-4 py-2 rounded-md text-lg dark:text-white dark:from-black dark:to-black" href="https://drive.google.com/file/d/1JGsI_js11UJGpBuvtI92cQpkpH7OmUb_/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a> </div>
+            <div className="mr-3"><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="text-cyan-500 cursor-pointer text-3xl transition ease-in-out delay-50 hover:scale-110 dark:text-slate-900" /></div>
           </nav>
 
           <div className="text-center pt-12">
-            <h2 className="text-6xl text-purple-500 font-medium dark:text-black">Mehul Aneja</h2> 
-            <h3 className="text-2xl pt-5 text-purple-400 dark:text-slate-900">Contact: mehulaneja123@gmail.com</h3>
-            <h3 className="text-2xl pb-10 text-purple-400 dark:text-slate-900">Bachelors in Computer Science & Mathematics @ New York University 2024</h3>
+            <h2 className="text-6xl text-cyan-500 font-medium dark:text-black">Mehul Aneja</h2> 
+            <h3 className="text-2xl pt-5 text-cyan-400 dark:text-slate-900">Contact: mehulaneja123@gmail.com</h3>
+            <h3 className="text-2xl pb-10 text-cyan-400 dark:text-slate-900">Bachelors in Computer Science & Mathematics @ New York University 2024</h3>
           </div>
 
           <div className="relative mx-auto rounded-full dark:border-black dark:border w-72 h-72 overflow-hidden">
